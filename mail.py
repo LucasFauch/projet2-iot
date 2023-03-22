d3a9fde7300738f204a3994ce2bc2f5a8561eef6
Quentin Pauwels <quentin.pauwels@hotmail.com>
2023-03-22 16:32:41 +0100

    commentaires plus clairs

--- mail.py
+++ mail.py
@@ -2,24 +2,20 @@ import smtplib
 import ssl
 from email.message import EmailMessage
 
-# Setup port number and servr name
-
-# Standard secure SMTP port
+# Mise en place smtp_port et smtp_server
 smtp_port = 587
 smtp_server = "smtp.gmail.com"
 
+# Informations sur destinataire et expéditeur
 email_from = "projet2iotuqac@gmail.com"
 email_to = "ekiam333@gmail.com"
-
 pswd = "qecmbyexhgjvtxoc"
 
+# Création du mail et des détails nécessaires
 msg = EmailMessage()
 
 body = "Alerte détection anormale de l'utilisation de votre CPU sur votre object connecté."
 end_mail = "Envoyé depuis {}".format(email_from)
-
-print(body)
-
 msg.set_content(body)
 
 msg.add_alternative("""<!DOCTYPE html>
@@ -35,25 +31,22 @@ msg['Subject'] = 'Subject TEST'
 msg['From'] = email_from
 msg['To'] = email_to
 
-# Create context
+# Préparation pour l'envoi du mail
 simple_email_context = ssl.create_default_context()
 
 try:
-    print("Connecting to the server...")
+    print("Connexion au serveur...")
     server = smtplib.SMTP(smtp_server, smtp_port)
     server.starttls(context=simple_email_context)
     server.login(email_from, pswd)
-    print("Connection to the server ok")
-    print()
-    print(f"Sending email to - {email_to}")
+    print("Connexion au serveur établie")
     #server.sendmail(email_from, email_to, message)
     server.send_message(msg)
-    print(f"Email successfully sent to - {email_to}")
+    print(f"Mail envoyé avec succès à {email_to}")
 
-# Print the error if there is one
 except Exception as e:
     print(e)
 
-# Close the port
+# Fermeture du port
 finally:
     server.quit()
